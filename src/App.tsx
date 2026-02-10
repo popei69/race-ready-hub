d373b699d0a3cd4e0c597da59f9a0a23fe360252
Benoit Pasquier <b.pasquier69@gmail.com>
2026-02-10 09:33:30 +0800

    Fix deployment basename

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner position="top-center" />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<MyRaces />} />
           <Route path="/race/new" element={<CreateRace />} />
